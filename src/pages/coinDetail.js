5aae181bd6062ba7e78f176a243054b35f2edc26
KANGDONGHWI <rkdehdgnl@naver.com>
2021-11-03 13:27:42 +0900

    Feat: coinDetail 페이지 구현

---
+++ src/pages/coinDetail.js
@@ -0,0 +1,78 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import CoinData from "../components/CoinData";
+import HistoryChart from "../components/HistoryChart";
+import coinGecko from "../apis/coinGecko";
+
+const coinDetail = () => {
+  const { id } = useParams();
+  const [coinData, setCoinData] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+
+  const formatData = (data) => {
+    return data.map((el) => {
+      return {
+        t: el[0],
+        y: el[1].toFixed(2),
+      };
+    });
+  };
+
+  useEffect(() => {
+    setIsLoading(true);
+    const fetchData = async () => {
+      const [day, week, year, detail] = await Promise.all([
+        coinGecko.get(`/coins/${id}/market_chart/`, {
+          params: {
+            vs_currency: "krw",
+            days: "1",
+          },
+        }),
+        coinGecko.get(`/coins/${id}/market_chart/`, {
+          params: {
+            vs_currency: "krw",
+            days: "7",
+          },
+        }),
+        coinGecko.get(`/coins/${id}/market_chart/`, {
+          params: {
+            vs_currency: "krw",
+            days: "365",
+          },
+        }),
+        coinGecko.get("/coins/markets/", {
+          params: {
+            vs_currency: "krw",
+            ids: id,
+          },
+        }),
+      ]);
+
+      setCoinData({
+        day: formatData(day.data.prices),
+        week: formatData(week.data.prices),
+        year: formatData(year.data.prices),
+        detail: detail.data[0],
+      });
+      setIsLoading(false);
+    };
+    fetchData();
+  }, []);
+
+  const renderData = () => {
+    if (isLoading) {
+      return <div>Loading....</div>;
+    }
+    return (
+      <div className="coinlist">
+        <HistoryChart data={coinData} />
+        <CoinData data={coinData.detail} />
+      </div>
+    );
+  };
+
+  return renderData();
+};
+
+export default coinDetail;
