118cb1b3d97f20d3633e9be3d1a511bf49698f3a
KANGDONGHWI <rkdehdgnl@naver.com>
2021-11-03 13:43:24 +0900

    Modify: a->div tag 변경

--- src/components/AddCoin.js
+++ src/components/AddCoin.js
@@ -34,13 +34,13 @@ const AddCoin = () => {
       <div className={isActive ? "dropdown-menu show" : "dropdown-menu"}>
         {availableCoins.map((el) => {
           return (
-            <a
+            <div
+              type="button"
               onClick={() => handleClick(el)}
-              href="#"
               className="dropdown-item"
             >
               {el}
-            </a>
+            </div>
           );
         })}
       </div>
