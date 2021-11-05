d7e769c8cd3b6b4a91a8c8f2ffd68ecd24de77ab
KANGDONGHWI <rkdehdgnl@naver.com>
2021-11-05 15:34:03 +0900

    Modify: header className 수정

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Header = () => {
-  return <h1 className="text-center text-warning mt-3 mb-4">Coinstar</h1>;
+  return <h1 className="header">CoinTracker</h1>;
 };
 
 export default Header;
