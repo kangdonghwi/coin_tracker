6dcb6d58721c10c4b11bbb17bd9f19f2040f3b82
KANGDONGHWI <rkdehdgnl@naver.com>
2021-11-05 15:35:56 +0900

    Feat: Home Favor Icon 추가

--- src/pages/coinSummary.js
+++ src/pages/coinSummary.js
@@ -2,20 +2,24 @@
 import React, { useState } from "react";
 import CoinList from "../components/CoinList";
 import FavoriteList from "../components/FavoriteList";
-import { BsStar } from "react-icons/bs";
+import { BsStar, BsHouse } from "react-icons/bs";
 
 const coinSummary = () => {
   const [favorite, setFavorite] = useState(false);
 
+  const handleGoHome = () => {
+    setFavorite(false);
+  };
   const handleClick = () => {
     setFavorite(!favorite);
   };
 
   return (
     <>
-      <button onClick={handleClick}>
-        <BsStar />
-      </button>
+      <div className="list-header">
+        <BsHouse onClick={handleGoHome} type="button" className="Home" />
+        <BsStar onClick={handleClick} type="button" />
+      </div>
       <div className="coin-app">
         {favorite ? <FavoriteList /> : <CoinList />}
       </div>
